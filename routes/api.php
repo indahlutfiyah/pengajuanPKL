11ee63f97c97b0001a0da5e0e0ce7e957b468e93
indahlutfiyah <indahlutfiyah30@gmail.com>
2022-07-27 11:34:19 +0700

    regis,login,logiut

--- routes/api.php
+++ routes/api.php
@@ -2,7 +2,7 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
-
+use App\Http\Controllers\AuthController;
 /*
 |--------------------------------------------------------------------------
 | API Routes
@@ -13,7 +13,9 @@ use Illuminate\Support\Facades\Route;
 | is assigned the "api" middleware group. Enjoy building your API!
 |
 */
-
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
-});
+// Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+    Route::post('/register',[AuthController::class,'register']);
+    Route::post('login',[AuthController::class,'login']);
+    Route::post('logout',[AuthController::class,'logout']);
+    Route::post('me', [AuthController::class,'me']);
+// });
--- routes/web.php
+++ routes/web.php
@@ -17,36 +17,36 @@ use Illuminate\Support\Facades\Route;
 
 
 Route::get('/', function () {
-    return view('login',$status=false,$msg="pesansalah");
+    return view("welcome");
 });
 
-Route::post('/auth/login', function () {
+// Route::post('/auth/login', function () {
     
-    // $email = 'UserAgent';
-    // $pass = 'pass';
+//     // $email = 'UserAgent';
+//     // $pass = 'pass';
 
-    // // checkuserpass
-    // $msg='';
+//     // // checkuserpass
+//     // $msg='';
 
-    // if('passsalah'){
-    //     $msg="Password Salah";
-    // }
+//     // if('passsalah'){
+//     //     $msg="Password Salah";
+//     // }
 
-    // if('passsalah'){
-    //     return view("login",['status'=>true,'msg'=>"Password Salah"]);
-    // }
+//     // if('passsalah'){
+//     //     return view("login",['status'=>true,'msg'=>"Password Salah"]);
+//     // }
 
-    // if('passsalah'){
-    //     return view("login",['status'=>true,'msg'=>"Password Salah"]);
-    // }
-    // return view("login",['status'=>true,'msg'=>$msg]);
+//     // if('passsalah'){
+//     //     return view("login",['status'=>true,'msg'=>"Password Salah"]);
+//     // }
+//     // return view("login",['status'=>true,'msg'=>$msg]);
    
 
-    // if("")
+//     // if("")
 
 
 
 
 
 
-});
+// });
