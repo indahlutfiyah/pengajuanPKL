a6b0d851d1744971e0d5f271ddaef7e116fec8fc
9Pascallis <pascallishenoch@gmail.com>
2022-08-03 13:48:51 +0700

    public

---
+++ routes/view.php
@@ -0,0 +1,62 @@
+<?php
+
+use Faker\Provider\UserAgent;
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\AuthController;
+use App\Http\Controllers\PengajuanController;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+
+
+Route::get('/', function () {
+    return view('login',$status=false,$msg="pesansalah");
+});
+Route::post('/register',[AuthController::class,'register']);
+Route::post('/login',[AuthController::class,'login']);
+Route::post('/logout',[AuthController::class,'logout']);
+
+Route::get('/showPengajuan', [PengajuanContyroller::class,'index']);
+Route::post('/createPengajuan', [PengajuanController::class,'store']);
+Route::post('/ubahPengajuan', [PengajuanController::class,'update']);
+Route::post('/deletePengajuan', [PengajuanController::class,'destroy']);
+
+Route::post('/auth/login', function () {
+    
+    // $email = 'UserAgent';
+    // $pass = 'pass';
+
+    // // checkuserpass
+    // $msg='';
+
+    // if('passsalah'){
+    //     $msg="Password Salah";
+    // }
+
+    // if('passsalah'){
+    //     return view("login",['status'=>true,'msg'=>"Password Salah"]);
+    // }
+
+    // if('passsalah'){
+    //     return view("login",['status'=>true,'msg'=>"Password Salah"]);
+    // }
+    // return view("login",['status'=>true,'msg'=>$msg]);
+   
+
+    // if("")
+
+
+
+
+
+
+});
--- routes/web.php
+++ routes/web.php
@@ -19,44 +19,44 @@ use App\Http\Controllers\PengajuanController;
 
 
 Route::get('/', function () {
-    return view('login',$status=false,$msg="pesansalah");
+    return 'welcome';
 });
-Route::post('/register',[AuthController::class,'register']);
-Route::post('/login',[AuthController::class,'login']);
-Route::post('/logout',[AuthController::class,'logout']);
+// Route::post('/register',[AuthController::class,'register']);
+// Route::post('/login',[AuthController::class,'login']);
+// Route::post('/logout',[AuthController::class,'logout']);
 
-Route::get('/showPengajuan', [PengajuanContyroller::class,'index']);
-Route::post('/createPengajuan', [PengajuanController::class,'store']);
-Route::post('/ubahPengajuan', [PengajuanController::class,'update']);
-Route::post('/deletePengajuan', [PengajuanController::class,'destroy']);
+// Route::get('/showPengajuan', [PengajuanContyroller::class,'index']);
+// Route::post('/createPengajuan', [PengajuanController::class,'store']);
+// Route::post('/ubahPengajuan', [PengajuanController::class,'update']);
+// Route::post('/deletePengajuan', [PengajuanController::class,'destroy']);
 
-Route::post('/auth/login', function () {
+// Route::post('/auth/login', function () {
     
-    // $email = 'UserAgent';
-    // $pass = 'pass';
+//     // $email = 'UserAgent';
+//     // $pass = 'pass';
 
-    // // checkuserpass
-    // $msg='';
+//     // // checkuserpass
+//     // $msg='';
 
-    // if('passsalah'){
-    //     $msg="Password Salah";
-    // }
+//     // if('passsalah'){
+//     //     $msg="Password Salah";
+//     // }
 
-    // if('passsalah'){
-    //     return view("login",['status'=>true,'msg'=>"Password Salah"]);
-    // }
+//     // if('passsalah'){
+//     //     return view("login",['status'=>true,'msg'=>"Password Salah"]);
+//     // }
 
-    // if('passsalah'){
-    //     return view("login",['status'=>true,'msg'=>"Password Salah"]);
-    // }
-    // return view("login",['status'=>true,'msg'=>$msg]);
+//     // if('passsalah'){
+//     //     return view("login",['status'=>true,'msg'=>"Password Salah"]);
+//     // }
+//     // return view("login",['status'=>true,'msg'=>$msg]);
    
 
-    // if("")
+//     // if("")
 
 
 
 
 
 
-});
+// });
