5c9a1b3925aa6386f4d14fbd582ab2dc0be209a2
mrtzaahna <ahnadamaramaritza@gmail.com>
2022-08-14 15:52:23 +0700

    tik

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -10,7 +10,7 @@ use Illuminate\Support\Facades\Auth;
 class AdminController extends Controller
 {
     /**
-     * Display a listing of the resource.
+     * Display a listing of the gatel.
      *
      * @return \Illuminate\Http\Response
      */
